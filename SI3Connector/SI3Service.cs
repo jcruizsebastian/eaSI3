9185e1af913a680a6829347c90cd2b774f9b3787
Jose Carlos Ruiz Sebastian <jcruiz@Ofvalencia.local>
2019-01-17 09:10:57 +0100

    Exito con la imputación de horas en SI3 a través de test

--- SI3Connector/SI3Service.cs
+++ SI3Connector/SI3Service.cs
@@ -15,7 +15,7 @@ namespace SI3Connector
 
         HttpClient client { get; set; }
 
-        public SI3Service(string username, string passworkd)
+        public SI3Service(string username, string password)
         {
             this.username = username;
             this.password = password;
@@ -25,13 +25,21 @@ namespace SI3Connector
 
         public async Task<string> Login()
         {
+            var dict = new Dictionary<string, string>();
+            dict.Add("user", username);
+            dict.Add("pwd", password);
+            dict.Add("DSN", "GESOPENFINANCE");
+
             var request = new HttpRequestMessage(HttpMethod.Post, "http://si3.infobolsa.es/si3/asp/identificacion.asp");
+            request.Headers.Add("cache-control", "no-cache");
             request.Headers.Add("Pragma", "no-cache");
+            request.Headers.Add("Accept", "*/*");
+            request.Headers.Add("accept-encoding", "gzip, deflate");
             request.Headers.Add("Accept-Language", "es-ES,es;q=0.5");
             request.Headers.TryAddWithoutValidation("Content-Type", "application/x-www-form-urlencoded");
-            request.Content = new StringContent($"user={username}&pwd={password}&DSN=GESOPENFINANCE");
-
             
+            request.Content = new FormUrlEncodedContent(dict);
+
             var cookieContainer = new CookieContainer();
             using (var handler = new HttpClientHandler() { CookieContainer = cookieContainer })
             {
@@ -70,11 +78,17 @@ namespace SI3Connector
 
         public async Task<string> AddWorklog(string issueid, DateTime date, int time)
         {
+            var dict = new Dictionary<string, string>();
+            dict.Add("newTimeRecord", time.ToString());
+            dict.Add("newDate", $"{date.Day.ToString("D2")}/{date.Month.ToString("D2")}/{date.Year - 2000}");
+            dict.Add("timerecordtype", "R");
+
             var request = new HttpRequestMessage(HttpMethod.Post, $"http://si3.infobolsa.es/Si3/its/asp/newTimeRecord.asp?cod={issueid}&type=1");
             request.Headers.Add("Pragma", "no-cache");
             request.Headers.Add("Accept-Language", "es-ES,es;q=0.5");
             request.Headers.TryAddWithoutValidation("Content-Type", "application/x-www-form-urlencoded");
-            request.Content = new StringContent($"newTimeRecord={time}&newDate={date.Day.ToString("D2")}%2F{date.Month.ToString("D2")}%2F{date.Year - 2000}&timerecordtype=R");
+
+            request.Content = new FormUrlEncodedContent(dict);
 
             var cookieContainer = new CookieContainer();
             using (var handler = new HttpClientHandler() { CookieContainer = cookieContainer })
